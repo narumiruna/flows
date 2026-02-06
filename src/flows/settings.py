403ef70c9de064fa44f3fde12695624374af44b5
narumi <toucans-cutouts0f@icloud.com>
2026-02-07 04:23:17 +0800

    Add rich dependency and enhance settings configuration

--- src/flows/settings.py
+++ src/flows/settings.py
@@ -1,5 +1,8 @@
+from pydantic import Field
 from pydantic_settings import BaseSettings
+from pydantic_settings import PydanticBaseSettingsSource
 from pydantic_settings import SettingsConfigDict
+from pydantic_settings import YamlConfigSettingsSource
 
 
 class Settings(BaseSettings):
@@ -8,7 +11,21 @@ class Settings(BaseSettings):
         env_file_encoding="utf-8",
         case_sensitive=False,
         extra="forbid",
+        yaml_file=["config.yaml"],
     )
+
+    dataset_root: str = Field(default="~/data/ILSVRC2012")
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        return (YamlConfigSettingsSource(settings_cls),)
 
 
 settings = Settings()
