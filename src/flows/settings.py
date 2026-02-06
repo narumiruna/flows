9918b21ba44f8d3aa021bcc6e02949a0741cf19a
narumi <toucans-cutouts0f@icloud.com>
2026-02-06 22:14:46 +0800

    Refactor code structure for improved readability and maintainability

---
+++ src/flows/settings.py
@@ -0,0 +1,15 @@
+from attr import s
+from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="forbid",
+    )
+
+
+settings = Settings()
